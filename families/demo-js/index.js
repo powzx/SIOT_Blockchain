05bce08e1af4cb314363fbf137e528b03d79327e
zhixiang <pzhixiang.99@gmail.com>
2022-05-23 16:05:22 +0800

    Add instructions to run demo transaction processor with single validator

--- families/demo-js/index.js
+++ families/demo-js/index.js
@@ -1,3 +1,8 @@
+/*
+Run 'npm install' followed by 'node index.js' to run this demo transaction processor.
+It will attempt to connect to a validator at localhost:4004
+*/
+
 const { TransactionProcessor } = require('sawtooth-sdk/processor');
 const WalletHandler = require('./src/tp/demo_handler');
 
